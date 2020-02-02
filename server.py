cd717bb08fb3fd8c9905a1ad101b3405b9be0501
koerners <contact@skoerner.com>
2020-02-02 12:35:12 +0100

    API update

--- server.py
+++ server.py
@@ -16,13 +16,19 @@ class ItemStore(object):
     def items(self, request):
         for row in c.execute('SELECT * FROM `auslastung`'):
             self._items.append(row)
+        request.setHeader('Access-Control-Allow-Origin', '*')
         request.setHeader('Content-Type', 'application/json')
         return json.dumps(self._items)
 
-
-    @app.route('/var/<string:name>', methods=['GET'])
-    def get_item(self, request, name):
-        return name
+    @app.route('/bib/<string:name>/<string:limit>', methods=['GET'])
+    def get_item(self, request, name, limit):
+        self._items.clear();
+        fields = [name, limit]
+        for row in c.execute('SELECT * FROM `auslastung` where Ort = ? order by PK desc limit ?', fields):
+            self._items.append(row)
+        request.setHeader('Access-Control-Allow-Origin', '*')
+        request.setHeader('Content-Type', 'application/json')
+        return json.dumps(self._items)
 
 
 if __name__ == '__main__':
